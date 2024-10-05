f8e0d5af91b03b80bbfcd2ad8f637eab055e2d77
João Paulo Oliveira <jpmatoli@Joaos-MacBook-Pro.local>
2024-10-05 23:47:14 +0100

    Add project files

--- KendraConnector/obj/Debug/net8.0/KendraConnector.AssemblyInfo.cs
+++ KendraConnector/obj/Debug/net8.0/KendraConnector.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KendraConnector")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1d32720b13f5952020c2839cbdaf11f98e45746")]
 [assembly: System.Reflection.AssemblyProductAttribute("KendraConnector")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KendraConnector")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
